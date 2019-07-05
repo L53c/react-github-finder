0ada69ccf2ec38e3153d455376b4cb532fc5a31f
unknown <you@example.com>
2019-07-05 06:32:47 +0100

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this App</h1>
+            <p>App to search github users</p>
+            <p>Version: 1.0.0</p>
+            <a href="https://www.youtube.com/watch?v=RTbYDXB9k_M" target="_blank"  rel="noopener noreferrer" >Music</a>
+        </Fragment>
+    )
+}
+
+export default About
